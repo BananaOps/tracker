4dfa75e9b6106345c154e3b5c2bc5b4a5c251004
Jérémy PLANCKEEL <jeremy.planckeel@outlook.com>
2025-01-28 07:35:00 +0100

    fix: add end date for filter today

--- server/event.go
+++ server/event.go
@@ -169,6 +169,7 @@ func (e *Event) TodayEvents(
 
 	var todayFilter *v1alpha1.SearchEventsRequest = &v1alpha1.SearchEventsRequest{
 		StartDate: today + "T00:00:00Z",
+		EndDate: today + "T23:59:59Z",
 	}
 
 	filter, err := utils.CreateFilter(todayFilter)
